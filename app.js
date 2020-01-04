ef732884608b40190e30850c19b72463daf0fdd9
Paul Kang <paul@paul-kang.com>
2020-01-04 21:28:10 +0800

    add API 20

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var port = process.env.PORT || 5000
 var response;
 var inputParam;
 var memberData = [];
+var courseMember = [];
 var memberAlreadyExist = false;
 
 // express 設定
@@ -23,15 +24,15 @@ app.use(function (req, res, next) {
 //   API:01 ?API=01&UserId=12345&Name=小王&Gender=男&Birth=2019-01-01&Phone=095555555&ID=A120000000&Address=新竹市 東區 中央路
 //          加入會員 成功回應 "API:01 會員已存在" 或 "API:01 會員寫入成功"
 //
-//   API:10 ?API=10 (TODO)
+//   API:10 ?API=10
 //          讀取 courseData, 成功回應 JSON.stringify(courseData), 失敗回應 "API:10 courseData 讀取失敗"
-//   API:11 ?API=11 (TODO)
+//   API:11 ?API=11
 //          讀取 courseHistory, 成功回應 JSON.stringify(courseHistory), 失敗回應 "API:11 courseHistory 讀取失敗"
-//   API:12 ?API=12 (TODO)
+//   API:12 ?API=12
 //          讀取 courseMember, JSON.stringify(courseMember), 失敗回應 "API:12 courseHistory 讀取失敗"
 //
-//   API:20 ?API=20&userName&courseID (TODO)
-//          報名寫入 courseMember with  ["courseID", ["userName", "未繳費", "未簽到"]], 成功回應 "API:20 報名成功" 或 "API:20 報名失敗"
+//   API:20 ?API=20&UserName&CourseId
+//          報名寫入 courseMember with  ["courseID", ["userName", "未繳費", "未簽到"]], 成功回應 "API:20 會員報名成功" 或 "API:20 會員報名失敗"
 
 app.get('/', function (req, res) {
   //console.log(req.query);
@@ -40,10 +41,10 @@ app.get('/', function (req, res) {
 
   // 若無 API 參數，無效退出
   if (typeof inputParam.API == "undefined") {
-    console.log("API error");
-    response.send("API error");
+    console.log("Error: No API");
+    response.send("Error: No API");
     return 0;
-  } 
+  }    
   
   //console.log("API is ", inputParam.API);
   
@@ -67,7 +68,11 @@ app.get('/', function (req, res) {
     case "12":
       console.log("呼叫 API:12 讀取 courseMember");
       readCourseMember();  
-      break;      
+      break; 
+    case "20":
+      console.log("呼叫 API:20 報名寫入 courseMember");
+      writeCourseMember();  
+      break;       
     default:
       console.log("呼叫 未知API:"+inputParam.API);
       response.send("呼叫 未知API:"+inputParam.API);
@@ -268,4 +273,91 @@ function readCourseMember(){
     console.log("API:12 courseMember 讀取成功");
        
   });  
+}
+
+function writeCourseMember() {
+  // 檢查 UserName 和 CourseId ===========================================================
+  var errMsg = "";
+  //console.log(inputParam.UserName, inputParam.CourseId);
+  if (inputParam.UserName == undefined) {
+    console.log("UserName is undefined"); 
+    errMsg += "UserName is undefined";
+  }
+  
+  if (inputParam.CourseId == undefined) {
+    console.log("CourseId is undefined"); 
+    errMsg += " CourseId is undefined";
+  }
+  
+  if (errMsg != "") {
+    response.send(errMsg);  
+    return 0;
+  }
+  // ====================================================================================
+  
+  // 讀取目前 courseData
+  database.ref("users/林口運動中心/課程管理").once("value").then(function (snapshot) {
+    //console.log(snapshot.val());
+    //console.log("資料庫課程管理讀取完成");
+    var result = snapshot.val();
+    //console.log(result);
+    try {      
+      courseMember=[];
+      courseMember = JSON.parse(result.課程會員);
+      //console.log(courseMember);   
+    } catch (e) {
+      console.log("API:20 courseMember 讀取失敗");
+      response.send("API:20 courseMember 讀取失敗");      
+      return 0;
+    }
+    console.log("API:20 courseMember 讀取成功");
+    
+    var courseIndex=-1;
+    var userInCourse = false;
+    courseMember.forEach(function(course, index, array){
+      if (course[0]==inputParam.CourseId ){
+        //console.log("Course matched:", course[0]);
+        courseIndex = index;
+        if (course.length>1) {
+          for (var i=1; i< course.length; i++) {
+            //console.log(i, course[i]);
+            if (course[i][0]== inputParam.UserName){
+              //console.log(inputParam.UserName, "已經報名過 ", inputParam.CourseId);
+              //response.send("API:20 "+inputParam.UserName+" 已經報名過 "+inputParam.CourseId);   
+              userInCourse = true;
+              break;
+            }
+          }
+        }
+      }
+    });
+
+    if (userInCourse) {
+      console.log(inputParam.UserName, "已經報名過 ", inputParam.CourseId);
+      response.send("API:20 "+inputParam.UserName+" 已經報名過 "+inputParam.CourseId); 
+      return 0;
+    };
+    // CourseId 還沒被 UserName 報名過
+    // push to courseMember    
+    courseMember[courseIndex].push([inputParam.UserName, "未繳費", "未簽到"]);
+
+    // Write to Database
+    database.ref('users/林口運動中心/課程管理').set({
+      課程會員: JSON.stringify(courseMember),
+    }, function (error) {
+      if (error) {
+        console.log("API:20 會員報名失敗");
+        response.send("API:20 會員報名失敗");      
+      } else {
+        console.log("API:20 會員報名成功");
+        response.send("API:20 會員報名成功");
+      }
+
+    });
+    
+    // API 回應成功
+    //response.send("API:20 "+inputParam.UserName+" 報名 "+inputParam.CourseId+" 成功"); 
+    
+    
+  });    
 }
